cee3a6b6922dd4f552a7d1022b3732ecf2f3fa49
Daimon-22 <elvelandiar@unal.edu.co>
2023-04-10 16:44:57 -0500

    1

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,39 @@
+package comunicacion;
+
+public class Alfabeto extends Pictograma{
+    private String[] letras;
+    private String interpretacion;
+
+    public Alfabeto(String origen, String[] letras, String interpretacion) {
+        super(origen);
+        this.letras = letras;
+        this.interpretacion = interpretacion;
+    }
+
+    public String[] getLetras() {
+        return letras;
+    }
+
+    public void setLetras(String[] letras) {
+        this.letras = letras;
+    }
+
+    public String getInterpretacion() {
+        return interpretacion;
+    }
+
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
+
+    public int cantidadLetras(){
+        int cantidad=letras.length;
+        return cantidad;
+    }
+    public  String interpretacion(){
+        return interpretacion;
+    }
+    public String toString(){
+        return "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+    }
+}
